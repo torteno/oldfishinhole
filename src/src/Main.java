bff2a1fe548be5a577500705f0f5a300c78dff86
Jack <jackwilson09@crescentschool.org>
2025-01-21 23:19:27 -0500

    code :D

---
+++ src/src/Main.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int trout = scan.nextInt();
+        int pike = scan.nextInt();
+        int pickerel = scan.nextInt();
+        int total = scan.nextInt();
+        int maxtrout = total / trout;
+        int maxpike = total / pike;
+        int maxpickerel = total / pickerel;
+
+        for (int i = 0; i <= maxtrout; i++) {
+            for (int j = 0; j <= maxpike; j++) {
+                for (int k = 0; k <= maxpickerel; k++) {
+                    if (i * trout + j * pike + k * pickerel == total) {
+                        System.out.println(i + " Brown Trout, " + j + " Northern Pike, " + k + " Yellow Pickerel");
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
